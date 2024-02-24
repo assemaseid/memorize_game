26053edd9768c8a8b996a722e3a66a1b26a2f4c7
assemaseid <seidkarim601705@gmail.com>
2024-02-24 16:48:34 +0600

    CardView and isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,15 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundStyle(.tint)
         .padding()
     }
 }
+
+struct CardView: View {
+    var isFaceUp:Bool = false
+    var body: some View {
+        if isFaceUp{
+            ZStack (content:{
+                       RoundedRectangle(cornerRadius: 25.0)
+                           .foregroundColor(.white)
+                       RoundedRectangle(cornerRadius: 25.0)
+                           .strokeBorder(lineWidth: 5)
+                       Text("👻").font(.largeTitle)
+            })
+        }else{
+            RoundedRectangle(cornerRadius: 25.0)
+        }
+       
+    }
+}
+
+
+
+
+
+
 
 #Preview {
     ContentView()

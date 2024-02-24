73d1c0e34879b35164da64af1f3147a106e49609
assemaseid <seidkarim601705@gmail.com>
2024-02-25 02:51:26 +0600

    Removed CardView lines by using ForEach cardadder and remover just beautiful ordered code

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,23 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis: [String] = ["🦁","🐶","🦋","🦀"]
+    let emojis: [String] = ["🦁","🐶","🦋","🦀","🦕","🐘", "🐏","🦚","🫎","🐝"]
+    @State var cardCount:Int = 4
+
     
     var body: some View {
+        VStack{
+            cards
+            cardCountAdjasters
+            
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+        .padding()
+    }
+    
+    var cards: some View {
         HStack{
-                
-            ForEach(emojis.indices, id: \.self){ index in
+            ForEach(0..<cardCount, id: \.self){ index in
                 CardView(content: emojis[index])
 
             }
+            .foregroundStyle(.tint)
         }
-        .foregroundStyle(.tint)
-        .padding()
     }
+    
+    var cardCountAdjasters:some View {
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+    }
+    
+    var cardAdder: some View {
+        Button(action: {
+            if cardCount < emojis.count {
+                cardCount += 1
+            }
+        }, label: {
+            Image(systemName: "rectangle.stack.fill.badge.plus")
+        })
+    }
+    
+    var cardRemover:some View {
+        Button(action: {
+            if cardCount > 1 {
+                cardCount -= 1
+            }
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.minus.fill")
+        })
+    }
+    
 }
 
 struct CardView: View {
-    @State var isFaceUp = false
+    @State var isFaceUp = true
     let content:String
     var body: some View {
         ZStack {
